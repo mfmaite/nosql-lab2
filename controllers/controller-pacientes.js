94b1193a85cc510eed9cbdbd6d6695e8b2ff1e0c
Maite <martinezf.maite@gmail.com>
2024-10-24 18:37:52 -0300

    Mejora el manejo de errores

--- controllers/controller-pacientes.js
+++ controllers/controller-pacientes.js
@@ -6,17 +6,16 @@ async function crearPaciente(datos) {
   try {
     const existsCi = await ciAlreadyExists(ci);
 
-    if (existsCi) throw new Error('Ya existe un paciente creado con esa cédula');
+    if (existsCi) throw { message: 'Ya existe un paciente creado con esa cédula', status: 401 };
 
     return createPatient(nombre, apellido, ci, fecha_nacimiento, sexo, telefono, direccion, email);
   } catch (error) {
     console.error('Error al crear el paciente:', error);
     return {
       data: {
-        message: 'Hubo un error al crear el paciente',
-        error: error.message
+        error: error.message,
       },
-      status: 500
+      status: error.status,
     };
   }
 }
